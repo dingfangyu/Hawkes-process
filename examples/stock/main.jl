a80cf72a7f1a7f1ff36dfddae657d419f3aa9524
Arthur-99 <1224795746@qq.com>
2019-10-06 20:22:57 +0800

    add stock

---
+++ examples/stock/main.jl
@@ -0,0 +1,65 @@
+include("../../src/hp.jl")
+include("../../examples/fluctuation/dataLoader.jl")
+
+files = ["examples/stock/stockevent.txt"]
+data = load_data(files)
+
+const proportion = 0.95
+
+train_data = Array{Tuple, 1}()
+
+for d in data
+    t, e, f, T0, T = d
+
+    train_n = findlast(x -> (x < proportion * T), t)
+
+    push!(train_data, (t[1:train_n],
+                       e[1:train_n],
+                       f[1:train_n, :],
+                       0.0,
+                       proportion * T
+                       )
+         )
+end
+
+
+# model
+model = Hawkes(event_types_num=2, features_num=0)
+
+
+# train
+loss_his = Array{Float64, 1}()
+train!(model, data, loss_his; iterations=100)
+
+
+# plot
+using PyCall
+@pyimport matplotlib.pyplot as plt
+
+plt.imshow(model.alpha)
+plt.show()
+plt.imshow(model.mu)
+plt.show()
+
+# predict
+for i in 1:length(data)
+    predict(model, train_data[i], data[i][5] - train_data[i][5])
+
+    plt.subplot(211)
+    plt.hist(data[i][1], bins=200, range=(0.0, data[i][5]))
+    plt.axvline(x=train_data[i][5], ymin=0, color="orange")
+
+    plt.subplot(212)
+    plt.hist(train_data[i][1], bins=200, range=(0.0, data[i][5]))
+    plt.axvline(x=train_data[i][5], ymin=0, color="orange")
+
+    plt.savefig("examples/stock/predData/plot" * string(i - 1) * ".jpg")
+    plt.show()
+
+    # output
+    f = open("examples/fluctuation/predData/" * string(i - 1) * "pred.txt", "w")
+    for (t, e) in zip(train_data[i][1], train_data[i][2])
+        writedlm(f, [t e])
+    end
+    close(f)
+end
