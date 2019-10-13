c5492b4b471f01a9ec3a69600ad5f706a4cc3855
Arthur-99 <1224795746@qq.com>
2019-10-13 15:17:27 +0800

    new prediction of stock data with model.beta

--- examples/stock/main.jl
+++ examples/stock/main.jl
@@ -29,13 +29,14 @@ model = Hawkes(event_types_num=2, features_num=0)
 
 # train
 loss_his = Array{Float64, 1}()
-train!(model, data, loss_his; iterations=50)
+train!(model, data, loss_his; iterations=100)
 
 
 # plot
 using PyCall
 @pyimport matplotlib.pyplot as plt
 
+println(model.beta)
 plt.imshow(model.alpha)
 plt.show()
 plt.imshow(model.mu)
@@ -43,6 +44,7 @@ plt.show()
 
 # predict
 for i in 1:length(data)
+    print(i, ' ')
     predict(model, train_data[i], data[i][5] - train_data[i][5])
 
     plt.subplot(211)
